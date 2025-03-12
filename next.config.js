59dcfbc71dea505e0b10fef46f389040bc0de6a3
Luis Arguello <larguellol@unemi.edu.ec.>
2025-03-12 17:48:12 -0500

    Otro despliegue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
+  basePath:'/portafolio',
+  assetPrefix:'/portafolio/'
 };
 
 module.exports = nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,19 +21,15 @@ const poppins = Poppins({
 export const metadata = {
   title: "Portafolio",
   description: "Luis Arguello Lopez",
+  icons: {
+    icon: '/favicon.ico',  
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <Head>
-        {/* Establecer el favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        {/* También puedes agregar otros tamaños de favicon si lo necesitas */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      </Head>
+  
       <body className={`${poppins.variable} ${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
