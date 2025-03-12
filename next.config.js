cfd23040e332b42d00909cd8de2f84e8971f6a68
Luis Arguello <larguellol@unemi.edu.ec.>
2025-03-11 22:33:17 -0500

    Segundo despliegue

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: "/portafolio/",  // Cambia "portafolio" al nombre de tu repositorio
+  basePath: "/portafolio",      // Esto también es importante para el enrutamiento correcto
+  // Habilita la exportación estática
+  trailingSlash: true,  // Para evitar problemas con rutas, asegúrate de agregar una barra al final de las URL
+  images: {
+    unoptimized: true,  // Habilita imágenes no optimizadas, ya que estamos exportando estáticamente
+  },
+};
+
+module.exports = nextConfig;
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Esto es necesario para que el proyecto se sirva desde una subcarpeta en GitHub Pages
-  assetPrefix: "/portafolio/", // Cambia "portafolio" al nombre de tu repositorio
-  basePath: "/portafolio", // Esto también es importante para el enrutamiento correcto
-};
-
-module.exports = nextConfig;
